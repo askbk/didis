42313515b9fbf0fd842d23f1a6dc63e50c2127c4
ask <askbk@protonmail.com>
2023-07-27 11:19:36 +0200

    Rename

--- liststore.h
+++ liststore.h
@@ -16,22 +16,13 @@ typedef struct {
   int length;
 } List;
 
-typedef struct {
-  char **keys;
-  List **lists;
-  int count;
-} ListStore;
-
-ListStore *new_list_store(void);
 ReturnValue lists_lpush(KeyValueStore *kvs, kv_key list_name, char *element);
-ReturnValue list_store_lpop(KeyValueStore *kvs, char *list_name);
-ReturnValue list_store_rpush(KeyValueStore *kvs, char *list_name,
-                             char *element);
-ReturnValue list_store_rpop(KeyValueStore *kvs, char *list_name);
+ReturnValue lists_lpop(KeyValueStore *kvs, char *list_name);
+ReturnValue lists_rpush(KeyValueStore *kvs, char *list_name, char *element);
+ReturnValue lists_rpop(KeyValueStore *kvs, char *list_name);
 ReturnValue lists_length(KeyValueStore *kvs, char *list_name);
-ReturnValue list_store_move(KeyValueStore *kvs, char *source, char *destination,
-                            int wherefrom, int whereto);
-ReturnValue list_store_trim(KeyValueStore *kvs, char *list_name, int start,
-                            int end);
+ReturnValue lists_move(KeyValueStore *kvs, char *source, char *destination,
+                       int wherefrom, int whereto);
+ReturnValue lists_trim(KeyValueStore *kvs, char *list_name, int start, int end);
 
 #endif
